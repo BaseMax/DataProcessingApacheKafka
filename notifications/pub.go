f760f92de1fbb7dbf49fd7c973b46784eaf67b3c
alireza <alirezaarzehgar82@gmail.com>
2023-09-07 15:11:17 +0330

    notifications: add kafka producer mechanism on Notify function
    - Add kafka configurations on .env.example
    
    Signed-off-by: alireza <alirezaarzehgar82@gmail.com>

--- notifications/pub.go
+++ notifications/pub.go
@@ -4,19 +4,25 @@ import (
 	"encoding/json"
 
 	"github.com/labstack/echo/v4"
+	"github.com/segmentio/kafka-go"
 
 	"github.com/BaseMax/real-time-notifications-nats-go/models"
 )
 
 func Notify(activity models.Activity) *NotifErr {
-
 	if err := models.Create(&activity); err != nil {
 		return &NotifErr{Err: err.Err, HTTPError: err.HttpErr}
 	}
 
 	subject := CreateSubject(activity.RecieverID)
 	data, _ := json.Marshal(activity)
+
 	if err := nConn.Publish(subject, data); err != nil {
+		return &NotifErr{Err: err, HTTPError: *echo.ErrInternalServerError}
+	}
+
+	_, err := kConn.WriteMessages(kafka.Message{Value: data})
+	if err != nil {
 		return &NotifErr{Err: err, HTTPError: *echo.ErrInternalServerError}
 	}
 
